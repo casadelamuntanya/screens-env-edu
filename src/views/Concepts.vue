e3506de050f0f6957a9f7676f12f9fa480641f3d
markusand <mvilella@obsa.ad>
2020-11-25 13:25:17 +0100

    Create concepts navigable structure
    [+] Load concepts from Airtable
    [+] Every concepts has link to its children and related concepts
    [+] Concept hyerarchy. Can go up till root
    [+] History mode. Can go back to previous visited concepts

--- src/views/Concepts.vue
+++ src/views/Concepts.vue
@@ -1,14 +1,87 @@
 <template>
 	<section class="concepts">
+		<div v-if="selected" class="concept">
+			<header class="concept__header">
+				<figure class="cover">
+					<img :src="selected.bg[0].url">
+				</figure>
+				<ul class="concept__path">
+					<li><button @click="select()">Conceptes</button></li>
+					<li v-for="step in selected.path" :key="step.name">
+						<button @click="select(step)">{{ step.name }}</button>
+					</li>
+				</ul>
+				<h2 class="concept__name">{{ selected.name }}</h2>
+				<p class="concept__description">{{ selected.description }}</p>
+			</header>
+			<ul class="concept__children">
+				<li v-for="concept in selected.children" :key="concept.name">
+					<button @click="select(concept)">
+						{{ concept.name }}
+					</button>
+				</li>
+			</ul>
+			<ul class="concept__related">
+				<li v-for="concept in selected.related" :key="concept.name">
+					<button @click="select(concept)">
+						{{ concept.name }}
+					</button>
+				</li>
+			</ul>
+		</div>
+		<ul v-else class="concepts__root">
+			<li v-for="concept in root" :key="concept.name">
+				<button @click="select(concept)">{{ concept.name }}</button>
+			</li>
+		</ul>
 		<router-link to="/">&times;</router-link>
 	</section>
 </template>
 
 <script>
+import airtable from '../airtable';
+import config from '../config.yaml';
+
 export default {
 	name: 'Concepts',
 	data() {
-		return {};
+		return {
+			concepts: {},
+			history: [],
+		};
+	},
+	computed: {
+		root() {
+			return Object.values(this.concepts).filter(concept => !concept._parent);
+		},
+		selected() {
+			if (!this.history.length) return undefined;
+			const [{ _parent, _children = [], _related = [], ...rest }] = this.history.slice(-1);
+			const children = _children.map(item => this.concepts[item]);
+			const related = _related.map(item => this.concepts[item]);
+			const path = this.getPath(_parent);
+			return { ...rest, path, children, related };
+		},
+	},
+	beforeMount() {
+		airtable('concepts')
+			.select(config.airtable.concepts)
+			.eachPage((records, nextPage) => {
+				const concepts = records.reduce((acc, { id, fields }) => {
+					acc[id] = fields;
+					return acc;
+				}, this.concepts);
+				this.concepts = { ...concepts };
+				nextPage();
+			});
+	},
+	methods: {
+		select(item) {
+			this.history = item ? [...this.history, item] : [];
+		},
+		getPath([id] = []) {
+			return id ? [...this.getPath(this.concepts[id]._parent), this.concepts[id]] : [];
+		},
 	},
 };
 </script>
