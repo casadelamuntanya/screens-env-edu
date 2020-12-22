a98da5fe768b049fe63b9d013252f3b351eccbe7
markusand <mvilella@obsa.ad>
2020-12-22 17:16:42 +0100

    Load concepts from airtable and iterate map

--- src/views/Tour.vue
+++ src/views/Tour.vue
@@ -6,12 +6,35 @@
 
 <script>
 import { Map } from 'mapbox-gl';
+import airtable from '../airtable';
 import config from '../config.yaml';
 
 export default {
 	name: 'Tour',
 	data() {
-		return { map: undefined };
+		return {
+			map: undefined,
+			waypoints: undefined,
+			step: undefined,
+		};
+	},
+	computed: {
+		waypoint() {
+			const { waypoints, step } = this;
+			if (!waypoints) return undefined;
+			const waypoint = waypoints[step];
+			return waypoint;
+		},
+	},
+	watch: {
+		waypoint: {
+			deep: true,
+			handler({ lat, lon, zoom, bearing }) {
+				const { duration } = config.map;
+				const center = [lon, lat];
+				this.map.flyTo({ center, zoom, bearing, duration });
+			},
+		},
 	},
 	mounted() {
 		this.map = new Map({
@@ -21,6 +44,17 @@ export default {
 		});
 
 		config.map.disable.forEach(interaction => this.map[interaction].disable());
+		this.map.on('click', () => { this.step = (this.step + 1) % this.waypoints.length; });
+
+		this.map.on('load', () => {
+			airtable('tour')
+				.select(config.airtable.tour)
+				.firstPage()
+				.then(records => {
+					this.waypoints = records.map(({ fields }) => fields);
+					this.step = 0;
+				});
+		});
 	},
 };
 </script>
