bbb9043b9e23c87f8a68f718965dc4622ff871cb
markusand <mvilella@obsa.ad>
2020-12-28 13:25:49 +0100

    Add related items

--- src/views/Tour.vue
+++ src/views/Tour.vue
@@ -1,16 +1,28 @@
 <template>
 	<section class="tour">
 		<div id="map" />
+		<div v-if="waypoint">
+			<h1>{{ waypoint.name }}</h1>
+			<p>{{ waypoint.description }}</p>
+			<scroller v-slot="{ item }" :items="waypoint.related">
+				<card :item="item" attribution @click.native.stop>
+					<h3>{{ item.name }}</h3>
+				</card>
+			</scroller>
+		</div>
 	</section>
 </template>
 
 <script>
 import { Map } from 'mapbox-gl';
 import airtable from '../airtable';
+import Scroller from '../components/Scroller.vue';
+import Card from '../components/Card.vue';
 import config from '../config.yaml';
 
 export default {
 	name: 'Tour',
+	components: { Scroller, Card },
 	data() {
 		return {
 			map: undefined,
@@ -42,19 +54,36 @@ export default {
 			style: config.map.layer,
 			accessToken: process.env.MAPBOX_ACCESS_TOKEN,
 		});
-
 		config.map.disable.forEach(interaction => this.map[interaction].disable());
 		this.map.on('click', () => { this.step = (this.step + 1) % this.waypoints.length; });
-
-		this.map.on('load', () => {
-			airtable('tour')
-				.select(config.airtable.tour)
-				.firstPage()
-				.then(records => {
-					this.waypoints = records.map(({ fields }) => fields);
-					this.step = 0;
+		this.map.on('load', () => { this.fetchWaypoints(); });
+	},
+	methods: {
+		async fetchWaypoints() {
+			const records = await airtable('tour').select(config.airtable.tour).all();
+			const linked = await this.fetchLinked(records, 'related');
+			this.waypoints = records.map(({ fields }) => {
+				const related = fields.related.map(field => linked[field]);
+				return { ...fields, related };
+			});
+			this.step = 0;
+		},
+		async fetchLinked(records, field) {
+			const filterByFormula = `OR(${
+				records
+					.reduce((acc, record) => [...new Set([...acc, ...record.fields[field]])], [])
+					.map(id => `RECORD_ID()='${id}'`)
+					.join(',')
+			})`;
+			const linked = {};
+			await airtable('concepts')
+				.select({ ...config.airtable.concepts, filterByFormula })
+				.eachPage((results, nextPage) => {
+					results.forEach(({ id, fields }) => { linked[id] = fields; });
+					nextPage();
 				});
-		});
+			return linked;
+		},
 	},
 };
 </script>
