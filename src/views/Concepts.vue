133c58fda15b7279b521035048b6b1b88ef732f5
markusand <mvilella@obsa.ad>
2020-12-29 12:03:11 +0100

    Adapt Conceptes to SuperCard component

--- src/views/Concepts.vue
+++ src/views/Concepts.vue
@@ -1,70 +1,35 @@
 <template>
 	<section class="concepts">
-		<card
+		<super-card
 			v-if="selected"
 			:item="selected"
-			:class="['concept', { 'expanded': expanded }]"
-			attribution
-			@click.native="toggleExpandedContent">
-			<header class="concept__header">
-				<ul class="concept__path">
-					<li><button @click.stop="select()">Conceptes</button></li>
-					<li v-for="step in selected.path" :key="step.name">
-						<button @click.stop="select(step)">{{ step.name }}</button>
-					</li>
-				</ul>
-				<h2 class="concept__name">{{ selected.name }}</h2>
-				<p class="concept__description expandable">{{ selected.description }}</p>
-			</header>
-			<footer>
-				<div v-if="selected.media" class="concept__media expandable">
-					<scroller v-slot="{ item }" :items="selected.media">
-						<card :item="item" attribution @click.native.stop />
-					</scroller>
-				</div>
-				<div class="concept__children">
-					<scroller v-slot="{ item }" :items="selected.children">
-						<card :item="item" @click.native.stop="select(item)">
-							<h3>{{ item.name }}</h3>
-						</card>
-					</scroller>
-				</div>
-				<div v-if="selected.related.length" class="concept__related">
-					<h4>També et pot interessar...</h4>
-					<scroller v-slot="{ item }" :items="selected.related">
-						<card :item="item" @click.native.stop="select(item)">
-							<h3>{{ item.name }}</h3>
-						</card>
-					</scroller>
-				</div>
-			</footer>
-		</card>
+			patheable
+			@select="select" />
 		<ul v-else class="concepts__root">
 			<li v-for="concept in root" :key="concept.name">
 				<card :item="concept" @click.native.stop="select(concept)">
-					<h3>{{ concept.name }}</h3>
+					<h1>{{ concept.name }}</h1>
 				</card>
 			</li>
 		</ul>
-		<router-link to="/" class="close-btn">&times;</router-link>
+		<router-link v-if="!selected" to="/" class="close-btn">&times;</router-link>
 	</section>
 </template>
 
 <script>
-import Scroller from '../components/Scroller.vue';
 import Card from '../components/Card.vue';
+import SuperCard from '../components/SuperCard.vue';
 import airtable from '../airtable';
 import config from '../config.yaml';
 import { normalize } from '../utils/utils.string';
 
 export default {
 	name: 'Concepts',
-	components: { Scroller, Card },
+	components: { Card, SuperCard },
 	data() {
 		return {
 			concepts: {},
 			history: [],
-			expanded: false,
 		};
 	},
 	computed: {
@@ -99,13 +64,9 @@ export default {
 	methods: {
 		select(item) {
 			this.history = item ? [...this.history, item] : [];
-			this.expanded = false;
 		},
 		getPath([id] = []) {
 			return id ? [...this.getPath(this.concepts[id]._parent), this.concepts[id]] : [];
-		},
-		toggleExpandedContent() {
-			this.expanded = !this.expanded;
 		},
 	},
 };
